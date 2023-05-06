f4e4528a32c23acf38445db72501791095d552b3
9OSU1G02 <huyquocnguyen1902@gmail.com>
2023-05-06 21:19:57 +0700

    create project

---
+++ app/src/main/java/com/gosu1902/retrofittodos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.gosu1902.retrofittodos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
